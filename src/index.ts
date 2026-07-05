5836bd6d8c48e720bbf35541091f5eb0b5cc3ed0
altrup <51763643+altrup@users.noreply.github.com>
2026-07-05 01:03:00 -0400

    properly rename

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { EmbedRouter } from "./EmbedRouter";
 export { RouteButtonBuilder } from "./componentBuilders/RouteButtonBuilder";
-export { RouteStringSelectMenuBuilder as RouteSelectMenuBuilder } from "./componentBuilders/RouteStringSelectMenuBuilder";
-export { RouteStringSelectMenuOptionBuilder as RouteSelectMenuOptionBuilder } from "./componentBuilders/RouteStringMenuOptionBuilder";
+export { RouteStringSelectMenuBuilder } from "./componentBuilders/RouteStringSelectMenuBuilder";
+export { RouteStringSelectMenuOptionBuilder } from "./componentBuilders/RouteStringMenuOptionBuilder";
 
 export type * from "./types/routes";
