ef464c37942bccecb4bd2cef1f0306fda345c5dd
altrup <51763643+altrup@users.noreply.github.com>
2026-07-04 20:44:04 -0400

    embed router hooks

--- src/EmbedRouter.ts
+++ src/EmbedRouter.ts
@@ -7,8 +7,18 @@ import {
 	stringify,
 	TokenData,
 } from "path-to-regexp";
-import { CompiledRoute, RouteHandler } from "./types/routes";
+import {
+	CompiledRoute,
+	ResolvedRoute,
+	RouteHandler,
+	State,
+} from "./types/routes";
 import { ExtractParams } from "./types/ExtractParams";
+import {
+	AutocompleteInteraction,
+	Interaction,
+	InteractionReplyOptions,
+} from "discord.js";
 
 export default class EmbedRouter {
 	// single path -> RouteHandler
@@ -30,19 +40,84 @@ export default class EmbedRouter {
 
 	// command for adding a subrouter
 	use<P extends Path = Path>(routePath: P, router: EmbedRouter) {
-		const pathString = stringify(
-			routePath instanceof TokenData ? routePath : parse(routePath),
-		);
+		const pathString = this.pathToString(routePath);
 		for (const route of router.routes) {
 			this.on(
 				route.path.map((p) =>
-					path.posix.join(
-						pathString,
-						stringify(p instanceof TokenData ? p : parse(p)),
-					),
+					path.posix.join(pathString, this.pathToString(p)),
 				),
 				route.handler,
 			);
 		}
+	}
+
+	// command for using router
+	async handleInteraction<P extends Path = Path>(
+		interaction: Interaction,
+		initialPath: P,
+		flags?: InteractionReplyOptions["flags"],
+	) {
+		if (interaction.isAutocomplete()) {
+			throw new Error("Autocomplete Interactions aren't supported");
+		}
+
+		return await this.updateInteraction(interaction, initialPath, flags);
+	}
+
+	private async updateInteraction<P extends Path = Path>(
+		interaction: Exclude<Interaction, AutocompleteInteraction>,
+		initialPath: P,
+		flags?: InteractionReplyOptions["flags"],
+	) {
+		const resolvedRoute = this.resolve(initialPath);
+		if (!resolvedRoute) {
+			throw new Error(`No route found for ${this.pathToString(initialPath)}`);
+		}
+
+		const routeResponse = resolvedRoute.handler(
+			interaction,
+			resolvedRoute.state,
+		);
+
+		if (interaction.replied || interaction.deferred) {
+			if (flags) {
+				throw new Error(
+					"You can only set flags for interactions that haven't been replied to",
+				);
+			}
+			await interaction.editReply(routeResponse);
+		} else if ("update" in interaction) {
+			if (flags) {
+				throw new Error(
+					"You can only set flags for interactions that haven't been replied to",
+				);
+			}
+			await interaction.update(routeResponse);
+		} else {
+			interaction.reply({
+				...(routeResponse as InteractionReplyOptions),
+				flags,
+			});
+		}
+	}
+
+	private resolve<P extends Path = Path>(
+		routePath: P,
+	): ResolvedRoute<ExtractParams<P>> | null {
+		const pathString = this.pathToString(routePath);
+		for (const route of this.routes) {
+			const result = route.matchFunction(pathString);
+			if (result) {
+				return {
+					state: result as State<ExtractParams<P>>,
+					handler: route.handler,
+				};
+			}
+		}
+		return null;
+	}
+
+	private pathToString<P extends Path>(path: P): string {
+		return stringify(path instanceof TokenData ? path : parse(path));
 	}
 }
--- src/types/routes.ts
+++ src/types/routes.ts
@@ -1,20 +1,21 @@
-import {
-	Interaction,
-	InteractionReplyOptions,
-	MessagePayload,
-} from "discord.js";
+import { Interaction, InteractionEditReplyOptions } from "discord.js";
 import { MatchFunction, MatchResult, ParamData, Path } from "path-to-regexp";
 
 export type State<P extends ParamData> = MatchResult<P> & {
 	urlParams: Record<string, string>;
 };
-export type RouteResponse = string | MessagePayload | InteractionReplyOptions;
+export type RouteResponse = InteractionEditReplyOptions;
 export type RouteHandler<P extends ParamData> = (
 	interaction: Interaction,
 	state: State<P>,
 ) => RouteResponse;
+
 export type CompiledRoute<P extends ParamData> = {
 	path: Path[];
 	matchFunction: MatchFunction<P>;
-	handler: RouteHandler<ParamData>;
+	handler: RouteHandler<P>;
+};
+export type ResolvedRoute<P extends ParamData> = {
+	state: State<P>;
+	handler: RouteHandler<P>;
 };
